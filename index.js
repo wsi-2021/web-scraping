52e7fe2a19d2eee376b16a515c1e969a10c655cb
Karl Stolley <karl.stolley@gmail.com>
2021-02-24 16:18:42 -0600

    Save a JSON representation of all course titles

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ const cheerio = require('cheerio');
 var raw_html = fs.readFileSync('data/raw/itmd.html');
 var $ = cheerio.load(raw_html);
 
+var courses = [];
+
 console.log("There are", $('.coursetitle').length, "ITMD courses. And here they are:");
 
 $('.coursetitle').each(function() {
-  console.log($(this).text());
+  var course = {};
+  course['title'] = $(this).text();
+  courses.push(course);
+  //console.log($(this).text());
 });
 
+console.log(courses);
+
 // $('.courseblockdesc').text().replace(/\n/gm, "");
+
+fs.writeFileSync('data/itmd.json', JSON.stringify(courses));
